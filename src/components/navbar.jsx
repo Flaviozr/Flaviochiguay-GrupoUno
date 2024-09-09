0ee2c100930f3d50146742e77a2d717fe06215d3
Flaviozr <142418499+Flaviozr@users.noreply.github.com>
2024-09-08 22:30:54 -0300

    Actualizar navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import './Navbar.css'; // Mantén tus estilos personalizados si los tienes
+import './Navbar.css';
 
 function CustomNavbar() {
     return (
